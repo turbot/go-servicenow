ddc2721dc2d91d0dec410febf85aa6ee76fb1379
luis cavalcante <luis@turbot.com>
2023-03-28 00:30:11 -0300

    Add offset parameter to Table API

--- servicenow/now_table.go
+++ servicenow/now_table.go
@@ -17,11 +17,12 @@ func newNowTable(sn *ServiceNow) *NowTable {
 // List all consumers.
 //
 // See: https://docs.servicenow.com/bundle/tokyo-application-development/page/integrate/inbound-rest/concept/c_TableAPI.html#title_table-GET
-func (sn *NowTable) List(tableName string, limit int, result interface{}) error {
+func (sn *NowTable) List(tableName string, limit int, offset int, result interface{}) error {
 	endpointUrl := sn.baseURL.JoinPath(fmt.Sprintf("api/now/table/%s", tableName))
 
 	queryUrl := endpointUrl.Query()
 	queryUrl.Add("sysparm_limit", strconv.Itoa(limit))
+	queryUrl.Add("sysparm_offset", strconv.Itoa(offset))
 	endpointUrl.RawQuery = queryUrl.Encode()
 
 	method := "GET"
