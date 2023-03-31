6b6e951c1c1f60d2a081a2278eba50ff2e0fb68f
luis cavalcante <luis@turbot.com>
2023-03-31 15:28:25 -0300

    Add query param to Table API

--- servicenow/now_table.go
+++ servicenow/now_table.go
@@ -16,12 +16,15 @@ func newNowTable(sn *ServiceNow) *NowTable {
 // List all consumers.
 //
 // See: https://docs.servicenow.com/bundle/tokyo-application-development/page/integrate/inbound-rest/concept/c_TableAPI.html#title_table-GET
-func (sn *NowTable) List(tableName string, limit int, offset int, result interface{}) error {
+func (sn *NowTable) List(tableName string, limit int, offset int, query string, result interface{}) error {
 	endpointUrl := sn.baseURL.JoinPath(fmt.Sprintf("api/now/table/%s", tableName))
 
 	queryUrl := endpointUrl.Query()
 	queryUrl.Add("sysparm_limit", strconv.Itoa(limit))
 	queryUrl.Add("sysparm_offset", strconv.Itoa(offset))
+	if query != "" {
+		queryUrl.Add("sysparm_query", query)
+	}
 	endpointUrl.RawQuery = queryUrl.Encode()
 
 	return sn.doAPI("GET", endpointUrl.String(), result)
