195a5edfe7c26e7b1b328e41c7ef96461c51b059
luis cavalcante <luis@turbot.com>
2023-04-26 00:53:44 -0300

    Create function to handle sn_chg_rest/change/schedule endpoint

--- servicenow/sn_chg_rest_change_schedule.go
+++ servicenow/sn_chg_rest_change_schedule.go
@@ -2,7 +2,6 @@ package servicenow
 
 import (
 	"fmt"
-	"strconv"
 )
 
 type SnChgRestChangeSchedule struct {
@@ -13,19 +12,10 @@ func newSnChgRestChangeSchedule(sn *ServiceNow) *SnChgRestChangeSchedule {
 	return &SnChgRestChangeSchedule{sn}
 }
 
-// List all change schedules.
+// Get change schedule.
 //
-// See: https://docs.servicenow.com/bundle/rome-application-development/page/integrate/inbound-rest/concept/change-management-api.html#title_change-GET-schedule
-func (sn *SnChgRestChangeSchedule) List(changeSysId string, limit int, offset int, query string, result interface{}) error {
+// See: https://docs.servicenow.com/bundle/rome-application-development/page/integrate/inbound-rest/concept/change-management-api.html#title_change-GET-sched
+func (sn *SnChgRestChangeSchedule) Get(changeSysId string, result interface{}) error {
 	endpointUrl := sn.baseURL.JoinPath(fmt.Sprintf("api/sn_chg_rest/change/%s/schedule", changeSysId))
-
-	queryUrl := endpointUrl.Query()
-	queryUrl.Add("sysparm_limit", strconv.Itoa(limit))
-	queryUrl.Add("sysparm_offset", strconv.Itoa(offset))
-	if query != "" {
-		queryUrl.Add("sysparm_query", query)
-	}
-	endpointUrl.RawQuery = queryUrl.Encode()
-
 	return sn.doAPI("GET", endpointUrl.String(), result)
 }
