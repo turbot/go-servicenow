81bbb2a94e8eeade901ee7f457420845a269269c
luis cavalcante <luis@turbot.com>
2023-05-10 19:29:19 -0300

    Change Table API to add option to skip reference link

--- servicenow/now_table.go
+++ servicenow/now_table.go
@@ -13,27 +13,28 @@ func newNowTable(sn *ServiceNow) *NowTable {
 	return &NowTable{sn}
 }
 
-// List all consumers.
+// List all tables.
 //
 // See: https://docs.servicenow.com/bundle/tokyo-application-development/page/integrate/inbound-rest/concept/c_TableAPI.html#title_table-GET
-func (sn *NowTable) List(tableName string, limit int, offset int, query string, result interface{}) error {
+func (sn *NowTable) List(tableName string, limit int, offset int, query string, excludeReferenceLink bool, result interface{}) error {
 	endpointUrl := sn.baseURL.JoinPath(fmt.Sprintf("api/now/table/%s", tableName))
 
 	queryUrl := endpointUrl.Query()
 	queryUrl.Add("sysparm_limit", strconv.Itoa(limit))
 	queryUrl.Add("sysparm_offset", strconv.Itoa(offset))
+	queryUrl.Add("sysparm_exclude_reference_link", strconv.FormatBool(excludeReferenceLink))
 	if query != "" {
 		queryUrl.Add("sysparm_query", query)
 	}
 	endpointUrl.RawQuery = queryUrl.Encode()
 
-	return sn.doAPI("GET", endpointUrl.String(), result)
+	return sn.doAPI("GET", endpointUrl.String(), &result)
 }
 
-// Read consumer details.
+// Read table details.
 //
 // See: https://docs.servicenow.com/bundle/tokyo-application-development/page/integrate/inbound-rest/concept/c_TableAPI.html#title_table-GET-id
-func (sn *NowTable) Read(tableName string, sysId string, result interface{}) error {
+func (sn *NowTable) Read(tableName string, sysId string, excludeReferenceLink bool, result interface{}) error {
 	endpointUrl := sn.baseURL.JoinPath(fmt.Sprintf("api/now/table/%s/%s", tableName, sysId))
-	return sn.doAPI("GET", endpointUrl.String(), result)
+	return sn.doAPI("GET", endpointUrl.String(), &result)
 }
